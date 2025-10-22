7670af611ef92e1d452975cd6ea0dde5b1537b36
John Martinez <johnmartinez0408@gmail.com>
2025-10-22 09:02:30 -0400

    removed unused lines of code

--- src/components/Pages/Cart/CartSummary.jsx
+++ src/components/Pages/Cart/CartSummary.jsx
@@ -2,13 +2,7 @@ import { useSelector } from "react-redux";
 
 const CartSummary = (props)=>{
     
-    const shipping= 5.9;
-    // const cartData = useSelector((state)=>{return state})
-    // const {price, quantity} = props.data;
-    // let items=0;
-    // for(let i=0; i<props.data.length; i++){
-    //     items = items + (props.data[i].price * props.data[i].quantity);
-    // }
+
     return(
         <>  
         <   div className="row">
